24fa6509db27d5a8214477e5b1f70aa984e60c96
Joaquin Requejo <joaquinrequejo18@gmail.com>
2025-01-12 00:03:59 -0500

    perf: added support for page search result limits

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -5,13 +5,21 @@ module.exports = async function searchProducts(query, maxResults) {
   // Validate the request parameters
   if(!query) throw new ServerError("No query provided", 400)
   if(query.length < 3) throw new ServerError("Query too short (min 3 characters required)", 400)
-  if(maxResults && (isNaN(maxResults) || maxResults < 1)) throw new ServerError("Invalid max results", 400)
+  
+  // Set the default max results per page to 45
+  let productsListLimit = 45
+  if(maxResults) {
+    if(isNaN(maxResults) || maxResults < 1) throw new ServerError("Invalid max results", 400)
+    if(maxResults <= 15) productsListLimit = 15
+    else if(maxResults <= 30) productsListLimit = 30
+  }
+
   const results = {}
   let page = 1 // Actual page
   let index = 0 // Index of the products
 
   // Get the results page
-  let $ = await getCheerioPage(`https://www.crisol.com.pe/catalogsearch/result/index/?p=${page}&q=${query}`)
+  let $ = await getCheerioPage(`https://www.crisol.com.pe/catalogsearch/result/index/?p=${page}&product_list_limit=${productsListLimit}&q=${query}`)
   do {
     // Get all the results
     $('li.item.product.product-item').each((i, el) => {
@@ -35,7 +43,7 @@ module.exports = async function searchProducts(query, maxResults) {
     })
     if(index >= maxResults) break
     // Get the next page results of the query
-    $ = await getCheerioPage(`https://www.crisol.com.pe/catalogsearch/result/index/?p=${++page}&q=${query}`)
+    $ = await getCheerioPage(`https://www.crisol.com.pe/catalogsearch/result/index/?p=${++page}&product_list_limit=${productsListLimit}&q=${query}`)
     // Do this while there are results in the page
   } while ($('li.item.product.product-item').length > 0)
 
