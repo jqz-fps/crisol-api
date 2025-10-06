b6649c40605c769d3816513b605e34245b3b406b
Joaquin Requejo <joaquinrequejo18@gmail.com>
2025-10-05 19:34:57 -0500

    fix: solve throw error

--- src/services/productService.js
+++ src/services/productService.js
@@ -9,7 +9,7 @@ export default async function getProduct(isbn) {
   // Get the results and pick the first one
   let $ = await getCheerioPage(`https://www.crisol.com.pe/catalogsearch/result/index/?q=${isbn}`)
   let product_url = $('li.item.product.product-item').find('.product-item-link').attr('href')
-  if(!product_url) return reqError(res, 404, "Product not found")
+  if(!product_url) throw new ServerError("Product not found", 404);
 
   // Get the product page and search the main data
   let page = await getCheerioPage(product_url)
